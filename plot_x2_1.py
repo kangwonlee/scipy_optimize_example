d557301e09bb43e318c41d3cdd6661327b9fcef5
KangWon LEE <kangwon@gmail.com>
2016-11-09 17:51:42 +0900

    -2 ~ +2 -> -3 ~ +3

--- plot_x2_1.py
+++ plot_x2_1.py
@@ -2,10 +2,12 @@ import pylab
 
 
 def main():
-    x = pylab.linspace(-2, 2, 101)
+    x = pylab.linspace(-3, 3, 101)
     y = x ** 2 + 1
     pylab.plot(x, y)
     pylab.grid(True)
+    pylab.xlim([-3, 3])
+    pylab.ylim([0, 6])
 
     pylab.savefig('plot_x2_1.png', dpi=300)
 
