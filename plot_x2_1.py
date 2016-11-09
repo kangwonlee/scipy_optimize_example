109f5f2316dcf008cc98fd9c130969ca1e2b4a33
KangWon LEE <kangwon@gmail.com>
2016-11-09 17:52:52 +0900

    added dot plot

--- plot_x2_1.py
+++ plot_x2_1.py
@@ -4,7 +4,12 @@ import pylab
 def main():
     x = pylab.linspace(-3, 3, 101)
     y = x ** 2 + 1
+
+    xd = pylab.arange(-3, 3+0.1, 0.2)
+    yd = xd ** 2 + 1
+
     pylab.plot(x, y)
+    pylab.plot(xd, yd, '.')
     pylab.grid(True)
     pylab.xlim([-3, 3])
     pylab.ylim([0, 6])
