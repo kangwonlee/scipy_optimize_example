4dd3a009b310433bde9d2be3ab990fd0200c84af
KangWon LEE <kangwon@gmail.com>
2016-11-30 17:30:07 +0900

    implemented cost function

--- optimization_example.py
+++ optimization_example.py
@@ -1,4 +1,20 @@
+import numpy as np
 import scipy.optimize as so
+
+
+def cost_function(w, array_y_measurement, mat_x):
+    a_hat, b_hat = w
+
+    mat_w = np.matrix([[a_hat],
+                       [b_hat]])
+
+    mat_y_estimation = mat_x * mat_w
+
+    error = array_y_measurement - np.array(mat_y_estimation).flatten()
+
+    cost = sum(error ** 2) / len(array_y_measurement)
+
+    return cost
 
 
 def main():
