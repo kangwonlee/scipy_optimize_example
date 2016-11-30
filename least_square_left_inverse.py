4e03676660fcb4b8c0a1b486485d6a7d7e0cd20a
KangWon LEE <kangwon@gmail.com>
2016-11-30 16:39:26 +0900

    added comments and legend

--- least_square_left_inverse.py
+++ least_square_left_inverse.py
@@ -4,19 +4,25 @@ import numpy.random as nr
 
 
 def main():
+    # original parameters
     a = 0.5
     b = 1.0
 
+    # independent varialbe
     x_array = np.linspace(-8, 8, 101)
+
+    # true signal (assumption)
     y_array = a * x_array + b
 
+    # measured signal. contaminated with Gaussian noise
     y_contaminated_array = y_array + nr.normal(size=np.shape(y_array))
 
-    plt.plot(x_array, y_array)
-    plt.plot(x_array, y_contaminated_array, '.')
+    plt.plot(x_array, y_array, label='ground truth')
+    plt.plot(x_array, y_contaminated_array, '.', label='measurement')
 
     plt.axis('equal')
     plt.grid(True)
+    plt.legend(loc=0)
     plt.show()
 
 
