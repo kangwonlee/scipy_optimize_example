cbbb1bb967adcbcd72625d595cecbbb7f431415d
KangWon LEE <kangwon@gmail.com>
2016-11-30 17:40:39 +0900

    implemented numerical optimization

--- optimization_example.py
+++ optimization_example.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.optimize as so
 
+import least_square_left_inverse as li
+
 
 def cost_function(w, array_y_measurement, mat_x):
     a_hat, b_hat = w
@@ -18,7 +20,13 @@ def cost_function(w, array_y_measurement, mat_x):
 
 
 def main():
-    help(so)
+    x_array, y_array, y_contaminated_array = li.prepare_data_points()
+
+    mat_x = li.x_array_to_mat(x_array)
+
+    initial_guess = np.array((0, 0))
+    result = so.minimize(cost_function, initial_guess, args=(y_contaminated_array, mat_x))
+    print('result:\n%s' % result)
 
 
 if __name__ == '__main__':
